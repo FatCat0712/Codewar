aeaf4a57f7a3fe25c872f5ae1fab5ae6fd3f1516
FatCat0712 <maisonhai456@gmail.com>
2024-02-15 09:52:44 +0700

    update 15/02/2024

--- Break_Camel_case.java
+++ Break_Camel_case.java
@@ -3,23 +3,17 @@ import java.util.Arrays;
 public class Break_Camel_case {
     public static void main(String[] args) {
         String s = "camelCasing";
-        int n = s.length();
-        StringBuilder result = new StringBuilder();
-        char[] input = s.toCharArray();
-        for(int i = 0; i < n; i++){
-            char c = s.charAt(i);
-            if(c >= 'A' && c <= 'Z'){
-                n++;
-                char[] characters = Arrays.copyOf(input,n);
-
-                for(int j = n - 1; j >= i; j--){
-                    characters[i + 1] = characters[i];
-                }
-                characters[i] = ' ';
-                result.append(Arrays.toString(characters));
+        char[] characters = s.toCharArray();
+        String result = "";
+        for(int i = 0; i < characters.length; i++){
+            if(characters[i] >= 'A' &&  characters[i] <= 'Z'){
+                result += " " +characters[i];
+                i++;
             }
+            result += characters[i] +"";
         }
         System.out.println(result);
+
 
 
     }
