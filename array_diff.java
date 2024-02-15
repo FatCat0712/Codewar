57713b807a0b4145b4aba057db3c6fd3898637ce
FatCat0712 <maisonhai456@gmail.com>
2024-02-15 07:07:47 +0700

    some errors weren't fixed

---
+++ array_diff.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class array_diff {
+    public static int[] arrayDiff(int[] a, int[] b) {
+        // Your code here
+        if(a.length == 0) {
+            return new int[]{};
+        }
+        else if (b.length == 0) {
+            return a;
+        }
+        else{
+            int[] frequency = new int[a.length + b.length];
+            String output = "";
+            for (int i : a) {
+                frequency[i]++;
+            }
+            for (int j : b) {
+                frequency[j]++;
+            }
+            for (int i = 0; i < frequency.length; i++) {
+                if (frequency[i] == 1) {
+                    output += i + " ";
+                }
+            }
+
+            String[] characters = output.split(" ");
+            int[] result = new int[characters.length];
+            for(int k = 0; k < characters.length; k++) {
+                result[k] = Integer.parseInt(characters[k]);
+            }
+            return result;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,2,2};
+        int[] b = {1};
+//        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(arrayDiff(a,b)));
+
+    }
+}
